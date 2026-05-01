5d5252b8daa53c0db6e1cfc6f6c6ef25953eb2a2
Siva Bharathy <95018559+siva-sparkout@users.noreply.github.com>
2026-05-01 19:20:40 +0530

    first commit

---
+++ assets/cursor-CNV-McjT.js
@@ -0,0 +1,3 @@
+import{w as o}from"./core-6bwLXoaU.js";import"./index-x_odu9q5.js";import"./index.es-C6VojgMU.js";import"./events-DQ172AOg.js";import"./index-nibyPLVP.js";const l=o` <svg fill="none" viewBox="0 0 13 4">
+  <path fill="currentColor" d="M.5 0h12L8.9 3.13a3.76 3.76 0 0 1-4.8 0L.5 0Z" />
+</svg>`;export{l as cursorSvg};
